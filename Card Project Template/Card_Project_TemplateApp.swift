5129e59a63b86526895a4f99c4737e4a0e7e6a43
Mark Nair <marknair@me.com>
2023-11-20 09:31:54 -0600

    Initial Commit

---
+++ Card Project Template/Card_Project_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_Project_TemplateApp.swift
+//  Card Project Template
+//
+//  Created by Mark Nair on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Card_Project_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card Project Template/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Card Project Template
+//
+//  Created by Mark Nair on 11/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
